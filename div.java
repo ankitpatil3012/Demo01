59db28e49c728a9a7f0b33a6684dab55a374bb7f
ankitpatil3012 <101618238+ankitpatil3012@users.noreply.github.com>
2022-03-15 12:09:05 +0530

    Add files via upload

---
+++ div.java
@@ -0,0 +1,13 @@
+class div{
+
+     static void div(int n1,int n2)
+     {
+       int result=n1/n2;
+      System.out.println(result);
+     }
+    
+    public static void main(String ar[])
+    {
+     div(10,2);
+    }
+}
